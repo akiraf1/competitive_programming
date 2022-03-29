70db6b2a5d5ae956d7b53c1c5a26d38a137ef3d1
akiraf1 <akirafukushima2128@gmail.com>
2022-03-29 16:44:32 +0900

    ABC245

---
+++ ABC/245/b.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    n = int(input())
+    a = list(map(int, input().split()))
+    for i in range(0, 2001):
+        if i not in a:
+            print(i)
+            break
